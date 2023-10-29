863d4251cd2f4d56cb4511281200ed84b87b1bab
Alexei <bertenev0586@gmail.com>
2023-10-29 18:00:17 +0300

    кнопка выхода и убрал outline у полей в логине и регистрации

--- includes/topBar.php
+++ includes/topBar.php
@@ -1,8 +1,17 @@
 <div class="topBar">
     <a href="index.php"><img class="homeIcon" src="assets/img/homeIcon.png" alt="Главная"></a>
+
     <?php
-    if (!empty($_SESSION['firstName'])) {
-        echo "<a href='loginPage.php'>Здравствуйте, <span>" . $_SESSION['firstName'] . "</span>!</a>";
+    if (!empty($_SESSION['firstName'])) {echo "
+        <div class='topBarMenu'>
+            <a>Здравствуйте, <span>" . $_SESSION['firstName'] . "</span>!</a>
+            <form style='margin: 0' method='post'>
+                <input type='radio' style='display: none' name='logout' value='1' checked>
+                <button type='submit' class='logoutButton'>выйти</button>
+            </form>
+            
+        </div>";
+        include "includes/userLogout.php";
     }
     else {
         echo "<a href='loginPage.php'>Войти</a>";
